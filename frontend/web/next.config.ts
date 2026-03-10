6b7152dae001f3861a9e326395a8c6e855e70299
Luiz <luizrdamata@gmail.com>
2026-03-09 22:45:14 -0300

    refactor(api): refine cart service behavior

--- frontend/web/next.config.ts
+++ frontend/web/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.digital.allugator.com",
+      },
+      {
+        protocol: "https",
+        hostname: "yacare-products-image.s3.sa-east-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
